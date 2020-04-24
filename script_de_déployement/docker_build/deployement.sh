d453ead24be50a4b438d27f71979b5041ce02944
diomenios <loui.arys@gmail.com>
2020-04-24 12:59:30 +0200

    corrections DNS

--- script_de_déployement/docker_build/deployement.sh
+++ script_de_déployement/docker_build/deployement.sh
@@ -1,15 +1,11 @@
-docker network create --subnet=172.20.0.0/24 internal-services
-docker network create --subnet=172.20.10.0/24 external-services
-docker network create --subnet=172.30.0.0/16 woody-toys
+docker network create --subnet=172.20.0.0/16 wt3
 
-docker run -d --rm --name=resolveur --net=woody-toys --ip=172.30.0.2 resolveur
-docker run -d --rm --name=soa-local --net=internal-services --ip=172.20.0.2 soa-local
-docker run -d --rm --name=soa-public --net=external-services --ip=172.20.10.2 soa-public
+docker run -d --rm --name=resolveur --net=wt3 --ip=172.20.0.2 resolveur
+docker run -d --rm --name=soa-public --net=wt3 --ip=172.20.0.3 soa-public
 
 
-docker run -d --rm --name=host1 --net=woody-toys --ip=172.30.10.2 --dns=172.30.0.2  dnsutils /bin/bash -c "while :; do sleep 10; done"
-docker run -d --rm --name=host2 --net=woody-toys --ip=172.30.11.2 --dns=172.30.0.2  dnsutils /bin/bash -c "while :; do sleep 10; done"
+docker run -d --rm --name=host1 --net=wt3 --ip=172.20.10.2 --dns=172.20.0.2  dnsutils /bin/bash -c "while :; do sleep 10; done"
+docker run -d --rm --name=host2 --net=wt3 --ip=172.20.11.2 --dns=172.20.0.2  dnsutils /bin/bash -c "while :; do sleep 10; done"
 
-docker exec -d dns-server /etc/init.d/bind9  start
-docker exec -d dns-server /etc/init.d/bind9  start
-docker exec -d dns-server /etc/init.d/bind9  start
+docker exec -d resolveur /etc/init.d/bind9  start
+docker exec -d resolveur /etc/init.d/bind9  start
