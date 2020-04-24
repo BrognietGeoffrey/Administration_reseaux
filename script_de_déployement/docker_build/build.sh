32dc3547035a06fc3365fe27d60a784d8dba0796
diomenios <loui.arys@gmail.com>
2020-04-24 14:49:28 +0200

    correction scripts

--- script_de_déployement/docker_build/build.sh
+++ script_de_déployement/docker_build/build.sh
@@ -1,3 +1,3 @@
-docker build -t resolveur ../../DNS/resolveur/.
-docker build -t soa-public ../../DNS/authoritative_only_server/.
-docker build -t dnsutils ../../hosts/ubuntu_dnsutils/.
+docker build -t resolveur DNS/resolveur/.
+docker build -t soa-public DNS/authoritative_only_server/.
+docker build -t dnsutils hosts/ubuntu_dnsutils/.
