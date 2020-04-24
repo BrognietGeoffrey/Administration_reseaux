f023185092a0a1d1c6d9d51869c5b0acc63ca85f
diomenios <loui.arys@gmail.com>
2020-04-24 16:07:09 +0200

    mise en forme de la structure de \'websites\'

---
+++ websites/www/server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+const http = require('http');
+
+var app = express();
+
+app.get('/', function(req, res) {
+    res.sendFile('/public/html/acceuil.html', {root: __dirname });
+})
+
+.use('/', express.static('public'))
+
+.use(function(req, res, next){
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(404).send('Page introuvable !');
+});
+
+
+app.listen(8080);
