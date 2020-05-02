8ee9f373dd96d5020624bf10dfe6ad0f0b97c701
diomenios <loui.arys@gmail.com>
2020-05-02 14:04:51 +0200

    test dns

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
-# Administration reseaux - Projet 
+# Administration reseaux - Projet
 Projet de second semestre du BAC2 - 2019-20120
+
 ***
-# Auteurs 
+
+# Déployer le projet
+
+* En local : cloner le projet puis utiliser le script Make.sh via la commande ***./Make.sh***
+
+***
+
+# Auteurs
 *Groupe* : **2TL2-3**
-  - Louis Arys - [@Diomenios](https://github.com/Diomenios) 
-  - Martin Perdeans - [@Martin-95](https://github.com/Martinp-95) 
+  - Louis Arys - [@Diomenios](https://github.com/Diomenios)
+  - Martin Perdeans - [@Martin-95](https://github.com/Martinp-95)
   - Jean-Michael Tang - [@jeanmtang](https://github.com/jeanmtang)
   - Geoffrey Brogniet - [@BrognietGeoffrey](https://github.com/BrognietGeoffrey)
   ***
-  
-
-
--- script_de_déployement/docker_build/deployement.sh
+++ script_de_déployement/docker_build/deployement.sh
@@ -1,11 +1,12 @@
-docker network create --subnet=172.20.0.0/16 wt3
+docker network create --subnet=172.20.0.0/24 wt
+docker network create --subnet=172.30.0.0/24 dmz
 
-docker run -d --rm --name=resolveur --net=wt3 --ip=172.20.0.2 resolveur
-docker run -d --rm --name=soa-public --net=wt3 --ip=172.20.0.3 soa-public
+docker run -d --rm --name=resolveur --net=wt --ip=172.20.0.2 -p resolveur
+docker run -d --rm --name=soa-public --net=dmz --ip=172.30.0.2 -p 22:22 soa-public
 
 
-docker run -d --rm --name=host1 --net=wt3 --ip=172.20.10.2 --dns=172.20.0.2  dnsutils /bin/bash -c "while :; do sleep 10; done"
-docker run -d --rm --name=host2 --net=wt3 --ip=172.20.11.2 --dns=172.20.0.2  dnsutils /bin/bash -c "while :; do sleep 10; done"
+docker run -d --rm --name=host1 --net=wt --ip=172.20.10.2 --dns=172.20.0.2  dnsutils /bin/bash -c "while :; do sleep 10; done"
+docker run -d --rm --name=host2 --net=wt --ip=172.20.11.2 --dns=172.20.0.2  dnsutils /bin/bash -c "while :; do sleep 10; done"
 
 docker exec -d resolveur /etc/init.d/bind9  start
 docker exec -d resolveur /etc/init.d/bind9  start
