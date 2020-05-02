aa66250b3a3c852bf0b72b0c12127e4a3faaeca5
diomenios <loui.arys@gmail.com>
2020-05-02 14:28:28 +0200

    résolution erreur dns

--- script_de_déployement/docker_build/deployement.sh
+++ script_de_déployement/docker_build/deployement.sh
@@ -1,8 +1,8 @@
-docker network create --subnet=172.20.0.0/24 wt
+docker network create --subnet=172.20.0.0/16 wt
 docker network create --subnet=172.30.0.0/24 dmz
 
-docker run -d --rm --name=resolveur --net=wt --ip=172.20.0.2 -p resolveur
-docker run -d --rm --name=soa-public --net=dmz --ip=172.30.0.2 -p 22:22 soa-public
+docker run -d --rm --name=resolveur --net=wt --ip=172.20.0.2 resolveur
+docker run -d --rm --name=soa-public --net=dmz --ip=172.30.0.2 -p 53:53 soa-public
 
 
 docker run -d --rm --name=host1 --net=wt --ip=172.20.10.2 --dns=172.20.0.2  dnsutils /bin/bash -c "while :; do sleep 10; done"
