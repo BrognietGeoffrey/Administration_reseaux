9f59f701bbcefdce957ce580a0380b2c52c72c13
root <root@vps797982.ovh.net>
2020-05-03 16:46:22 +0200

    correction bugs

--- script_de_déployement/docker_build/websites.sh
+++ script_de_déployement/docker_build/websites.sh
@@ -1,8 +1,8 @@
 docker build -t b2b websites/b2b/.
-docker build -t b2b websites/database/.
-docker build -t b2b websites/intranet/.
-docker build -t b2b websites/www/.
+docker build -t database websites/database/.
+docker build -t intranet websites/intranet/.
+docker build -t www websites/www/.
 
 docker run --name=b2b --net=dmz-swarm -p 3300:8080 -d b2b
-docker run --name=b2b --net=dmz-swarm -d database
-docker run --name=b2b --net=dmz-swarm -p 3400:8080 -d www
+docker run --name=database --net=dmz-swarm -d database
+docker run --name=www --net=dmz-swarm -p 3400:8080 -d www
--- websites/b2b/public/js/global.js
+++ websites/b2b/public/js/global.js
@@ -1,2 +1,2 @@
-const DOMAIN_IP = "localhost:8080"
+const DOMAIN_IP = "51.178.41.115:3300"
 //const DOMAIN_IP = "51.178.41.115"
