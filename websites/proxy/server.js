05d5bcfd96efaef5a12578d6334ce76189613630
root <root@vps797983.ovh.net>
2020-05-22 13:47:13 +0200

    micro-modif

--- websites/proxy/server.js
+++ websites/proxy/server.js
@@ -31,16 +31,15 @@ const options = {
   cert: fs.readFileSync('server.crt', 'utf8')
 };
 
-
-    https.createServer(options, function(req, res) {
-        if (req.headers.host === 'www.wt2-3.ephec-ti.be:8000') {
+https.createServer(options, function(req, res) {
+        if (req.headers.host === 'www.wt2-3.ephec-ti.be') {
             proxy_www.proxyRequest(req, res);
             proxy_www.on('error', function(err, req, res) {
                 if (err) console.log(err);
                 res.writeHead(500);
                 res.end('Oops, something went very wrong...');
             });
-        } else if (req.headers.host === 'b2b.wt2-3.ephec-ti.be:8000') {
+        } else if (req.headers.host === 'b2b.wt2-3.ephec-ti.be') {
             proxy_b2b.proxyRequest(req, res);
             proxy_b2b.on('error', function(err, req, res) {
                 if (err) console.log(err);
@@ -53,4 +52,4 @@ const options = {
                 res.end(req.headers.host);
 
       	}
-    }).listen(8000);
+}).listen(443);
