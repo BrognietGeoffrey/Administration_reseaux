33bc95b1e6d39f33ed1c3a104b1ede0ec66d581d
root <root@vps797983.ovh.net>
2020-05-22 13:36:47 +0200

    ajout de https dans proxy

--- websites/proxy/server.js
+++ websites/proxy/server.js
@@ -1,37 +1,56 @@
-var http = require('http'),
+
+var https = require('https'),
     httpProxy = require('http-proxy');
+
+var fs = require('fs');
 
 var proxy_www = new httpProxy.createProxyServer({
         target: {
             host: 'www',
             port: 8080
-        }
+        },
+	ssl: {
+	    key: fs.readFileSync('key.pem', 'utf8'),
+	    cert: fs.readFileSync('server.crt', 'utf8')
+	}
 });
 
 var proxy_b2b = new httpProxy.createProxyServer({
         target: {
             host: 'b2b',
             port: 8080
-        }
+        },
+	ssl: {
+	    key: fs.readFileSync('key.pem', 'utf8'),
+	    cert: fs.readFileSync('server.crt', 'utf8')
+	}
 });
 
-    http.createServer(function(req, res) {
-        if (req.headers.host === 'www.wt2-3.ephec-ti.be') {
+const options = {
+  key: fs.readFileSync('key.pem', 'utf8'),
+  cert: fs.readFileSync('server.crt', 'utf8')
+};
+
+
+    https.createServer(options, function(req, res) {
+        if (req.headers.host === 'www.wt2-3.ephec-ti.be:8000') {
             proxy_www.proxyRequest(req, res);
             proxy_www.on('error', function(err, req, res) {
                 if (err) console.log(err);
                 res.writeHead(500);
                 res.end('Oops, something went very wrong...');
             });
-        } else if (req.headers.host === 'b2b.wt2-3.ephec-ti.be') {
+        } else if (req.headers.host === 'b2b.wt2-3.ephec-ti.be:8000') {
             proxy_b2b.proxyRequest(req, res);
             proxy_b2b.on('error', function(err, req, res) {
                 if (err) console.log(err);
                 res.writeHead(500);
                 res.end('Oops, something went very wrong...');
-            });
+	    });
         }
         else{
-      	    console.log(req.headers.host);
+      	        res.writeHead(200);
+                res.end(req.headers.host);
+
       	}
-    }).listen(80);
+    }).listen(8000);
