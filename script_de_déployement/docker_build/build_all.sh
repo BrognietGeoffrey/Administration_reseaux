d0e56cf7d72bbd0ec81382f729a0ad53537ff3f8
root <root@vps797983.ovh.net>
2020-05-24 14:57:44 +0200

    ajout des scripts pour l'host-mail

--- script_de_déployement/docker_build/build_all.sh
+++ script_de_déployement/docker_build/build_all.sh
@@ -7,4 +7,5 @@ docker build -t proxy websites/proxy/.
 docker build -t resolveur DNS/resolveur/.
 docker build -t soa-public DNS/authoritative_only_server/.
 docker build -t dnsutils hosts/ubuntu_dnsutils/.
+docker build -t host-mail hosts/ubuntu_mail/.
 docker build -t intranet websites/intranet/.
